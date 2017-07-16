179925bbef79b9726f12c8e8b1dbb8faf3a0f223
luciano.junior <lucianovsjr@gmail.com>
2017-07-16 12:36:40 -0300

    start django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lucianovsjr/Documents/school-of-net/entrega-continua-com-heroku/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
